0acce5cf6e769353957ef7b717c7d6f077344f99
Color_yr <402067010@qq.com>
2023-11-26 17:14:12 +0800

    up

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -199,11 +199,24 @@ public class JREUtils {
         envMap.put("FORCE_VSYNC", String.valueOf(LauncherPreferences.PREF_FORCE_VSYNC));
 
         envMap.put("MESA_GLSL_CACHE_DIR", Tools.DIR_CACHE.getAbsolutePath());
-        //for virgl
+
         if (LOCAL_RENDERER != null) {
-            envMap.put("MESA_GL_VERSION_OVERRIDE", "4.6");
-            envMap.put("MESA_GLSL_VERSION_OVERRIDE", "460");
+            switch (LOCAL_RENDERER) {
+                case "vulkan_zink":
+                    envMap.put("MESA_GL_VERSION_OVERRIDE", "4.6");
+                    envMap.put("MESA_GLSL_VERSION_OVERRIDE", "460");
+                    break;
+                case "opengles3_virgl":
+                    envMap.put("MESA_GL_VERSION_OVERRIDE", "4.3");
+                    envMap.put("MESA_GLSL_VERSION_OVERRIDE", "430");
+                    break;
+                case "malihw_panfrost":
+                    envMap.put("MESA_GL_VERSION_OVERRIDE", "3.3");
+                    envMap.put("MESA_GLSL_VERSION_OVERRIDE", "330");
+                    break;
+            }
         }
+
         envMap.put("force_glsl_extensions_warn", "true");
         envMap.put("allow_higher_compat_version", "true");
         envMap.put("allow_glsl_extension_directive_midshader", "true");
